0913d0b793afffa6c5365d5ff23c4d5b689c63b2
AhmadZuiod <armousa19@cit.just.edu.jo>
2023-07-30 05:16:20 +0300

    Changes

--- portfolio/src/Component/Credentials/Skills.js
+++ portfolio/src/Component/Credentials/Skills.js
@@ -3,35 +3,62 @@ import "../../ComponentStyle/Credentails/Skills.css";
 import Skill from "./Skill";
 export default function Skills() {
   const skillsObject = [
-    { skillName: "HTML/CSS", skillPercent: "90%", labelColor: "orange" },
-    { skillName: "JavaScript", skillPercent: "90%", labelColor: "yellow" },
-    { skillName: "React", skillPercent: "90%", labelColor: "blue" },
-    { skillName: "Bootstrap", skillPercent: "85%", labelColor: "purple" },
-    { skillName: "Anguler.TS", skillPercent: "60%", labelColor: "red" },
-    { skillName: "C#", skillPercent: "90%", labelColor: "purple" },
+    {
+      skillName: "HTML/CSS",
+      skillPercent: "95%",
+      labelColor: "orange",
+      key: 1,
+    },
+    {
+      skillName: "JavaScript",
+      skillPercent: "90%",
+      labelColor: "yellow",
+      key: 2,
+    },
+    { skillName: "React", skillPercent: "85%", labelColor: "blue", key: 3 },
+    {
+      skillName: "Bootstrap",
+      skillPercent: "80%",
+      labelColor: "purple",
+      key: 4,
+    },
+    { skillName: "Anguler.TS", skillPercent: "60%", labelColor: "red", key: 5 },
+    { skillName: "C#", skillPercent: "90%", labelColor: "purple", key: 6 },
     {
       skillName: ".NET Core Web API",
+      skillPercent: "70%",
+      labelColor: "purple",
+      key: 7,
+    },
+    {
+      skillName: ".NET Core MVC",
       skillPercent: "90%",
       labelColor: "purple",
+      key: 8,
     },
-    { skillName: ".NET Core MVC", skillPercent: "%", labelColor: "purple" },
-    { skillName: "Entity FrameWork", skillPercent: "", labelColor: "purple" },
-    { skillName: "Node.js", skillPercent: "", labelColor: "green" },
-    { skillName: "Express", skillPercent: "", labelColor: "black" },
-    { skillName: "GIT", skillPercent: "", labelColor: "grey" },
-    { skillName: "MongoDB", skillPercent: "", labelColor: "brown" },
+    {
+      skillName: "Entity FrameWork",
+      skillPercent: "60%",
+      labelColor: "purple",
+      key: 9,
+    },
+    { skillName: "Node.js", skillPercent: "80%", labelColor: "green", key: 10 },
+    { skillName: "Express", skillPercent: "90%", labelColor: "black", key: 11 },
+    { skillName: "MongoDB", skillPercent: "70%", labelColor: "brown", key: 13 },
     {
       skillName: "Microsoft SQL Server",
-      skillPercent: "",
-      labelColor: "white",
+      skillPercent: "90%",
+      labelColor: "pink",
+      key: 14,
     },
   ];
   return (
     <div>
       <h2>Skills</h2>
-      <div>
+      <div className="skills-container">
         {skillsObject.map((item) => (
           <Skill
+            key={item.key}
             skillName={item.skillName}
             skillPercent={item.skillPercent}
             color={item.labelColor}
--- portfolio/src/Shared/ProgressBar.js
+++ portfolio/src/Shared/ProgressBar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../SharedStyle/ProgressBar.css";
 export default function ProgressBar(props) {
-  console.log(props.color);
+  console.log(props.width);
   return (
     <div class="progress-container">
       <div
         class="progress-line"
         style={{
-          width: `${props.width}rem`,
+          width: `${props.width}`,
           backgroundColor: `${props.color}`,
         }}></div>
     </div>
